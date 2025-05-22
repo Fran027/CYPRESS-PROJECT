05c91f37f3f843a2beb876698404feb7b9631731
Fran <francielemendes027@gmail.com>
2025-05-21 23:54:29 -0300

    novas alteracoes

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,5 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('localhost:3000')
+    cy.get('[data-test="signup"]').click()
   })
 })
